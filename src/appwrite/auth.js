369f0b9a7174e1939f27a1cf17c8a5f0b94b1d9a
Dheerajgupta9520 <dheeraj109gupta@gmail.com>
2025-06-24 14:58:17 +0530

    first commit

---
+++ src/appwrite/auth.js
@@ -0,0 +1,65 @@
+import confi from "./confi.js";
+import { Client, Account,ID } from "appwrite";
+
+export class AuthService{
+    client=new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(confi.appwriteUrl)
+            .setProject(confi.appwriteProjectId);
+        this.account= new Account(this.client);
+    }
+
+    async createAccount ({email,password,name}){
+        try{
+           const userAccount = await this.account.create(ID.unique(),email,password,name);
+            
+           if (userAccount) {
+                return this.login({email,password})
+           } else {
+                return userAccount;
+           }
+
+        }
+        catch(e){
+            throw e
+        }
+    }
+
+    async login ({email,password}){
+        try{
+            const userAccount = await this.account.createEmailPasswordSession(email,password);
+            return userAccount
+        }
+        catch(e){
+            throw e
+        }
+    }
+
+    async getCurrentUser(){
+        try{
+            const userAccount = await this.account.get();
+            return userAccount
+        }
+        catch(e){
+            throw e
+        }
+
+        
+    }
+
+    async logout(){
+        try{
+            const userAccount = await this.account.deleteSessions();
+            return userAccount
+        }
+        catch(e){
+            throw e
+        }
+    }
+}
+
+const authService =new AuthService();
+
+export default authService
